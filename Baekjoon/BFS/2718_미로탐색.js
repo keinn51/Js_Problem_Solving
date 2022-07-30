7cc62a6fd2a6dc5b4c2f34cff91a6c53c0369007
keinn51 <soogood97@gmail.com>
2022-07-30 18:40:56 +0900

    백준-미로탐색 코멘트 추가

--- Baekjoon/BFS/2718_미로탐색.js
+++ Baekjoon/BFS/2718_미로탐색.js
@@ -3,17 +3,20 @@ https://www.acmicpc.net/problem/2178
 */
 
 const input = require("fs").readFileSync("text.txt").toString().split("\n");
+// N과 M은 각각 높이와 폭
 const [N, M] = input[0].split(" ").map(function (a) {
   return parseInt(a);
 });
 
+// graph 는 map
 let graph = new Array(N).fill([]).map(() => new Array(M));
+// stamp 는 내가 걸어온 발자국을 나타내는 map
 let stamp = new Array(N).fill([]).map(() => new Array(M).fill(false));
-
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) graph[i][j] = input[i + 1][j];
 }
 
+// 상하좌우를 갈 수 있도록 두 개의 배열 설정
 const dx = [1, 0, -1, 0], dy = [0, 1, 0, -1];
 const needVisitArr = [];
 
@@ -40,6 +43,7 @@ function bfs()
         }
     }
   }
+  console.log(stamp);
   return stamp[N-1][M-1];
 }
 
--- Baekjoon/DFS/2718_미로탐색.js
+++ Baekjoon/DFS/2718_미로탐색.js
@@ -3,16 +3,18 @@ https://www.acmicpc.net/problem/2178
 */
 
 const input = require("fs").readFileSync("text.txt").toString().split("\n");
+// N과 M은 각각 높이와 폭
 const [N, M] = input[0].split(" ").map(function (a) {
   return parseInt(a);
 });
 
+// graph 는 map
 let graph = new Array(N).fill([]).map(() => new Array(M));
-
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) graph[i][j] = input[i + 1][j];
 }
 
+// 상하좌우를 갈 수 있도록 두 개의 배열 설정
 const dx = [1, 0, -1, 0], dy = [0, 1, 0, -1];
 let total = 10000;
 
@@ -20,6 +22,7 @@ function iter(a,b, cnt)
 {
   let nx, ny;
   graph[a][b] = 'V';
+  // 끝에 도달하면 리턴
   if (a === N - 1 && b === M - 1)
   {
     total = Math.min(total, cnt)
