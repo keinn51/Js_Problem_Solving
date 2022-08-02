6a464825a642b12db9434d527529798e5d61ae62
keinn51 <soogood97@gmail.com>
2022-08-03 03:05:38 +0900

    백준-1874 스택 수열 문제 해결

--- Baekjoon/스택/1874_스택_수열.js
+++ Baekjoon/스택/1874_스택_수열.js
@@ -1,9 +1,33 @@
 const input = require("fs").readFileSync("text.txt").toString().split("\n");
 const length = parseInt(input[0]),
-  startStack = [],
+  startStack = new Array(length)
+    .fill(0)
+    .map((_, i) => i + 1)
+    .reverse(),
   middleStack = [],
-  resultStack = [];
+  resultStack = [],
+  goalStack = [];
+let answer = "";
 
-for (let i = 1; i <= length; i++) startStack.push(parseInt(input[i], 10));
+for (let i = 1; i <= length; i++) goalStack.push(parseInt(input[i], 10));
 
-console.log(startStack);
+while (resultStack.length !== length) {
+  if (
+    !middleStack.length ||
+    middleStack[middleStack.length - 1] !== goalStack[resultStack.length]
+  ) {
+    if (startStack.length) {
+      middleStack.push(startStack.pop());
+      answer += "+\n";
+    } else {
+      console.log("NO");
+      break;
+    }
+  } else {
+    resultStack.push(middleStack.pop());
+    answer += "-";
+    if (resultStack.length !== length) answer += "\n";
+  }
+}
+
+if (resultStack.length === length) console.log(answer);
