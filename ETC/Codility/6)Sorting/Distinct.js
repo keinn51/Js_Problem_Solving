40ad467b99174162fd3aeb3996f6fcfb6c0e03ec
keinn51 <soogood97@gmail.com>
2022-08-17 05:06:01 +0900

    Codility NumberOfDiscIntersections 다시 풀어야함

---
+++ ETC/Codility/6)Sorting/Distinct.js
@@ -0,0 +1,9 @@
+function solution(A) {
+  return new Set(A).size;
+}
+
+// console.log(solution([2, 1, 1, 2, 3, 1]));
+// console.log(solution([-1, -1]));
+// console.log(
+//   solution(new Array(100000).fill(0).map((_, idx) => parseInt(idx, 10))),
+// );
---
+++ ETC/Codility/6)Sorting/NumberOfDiscIntersections.js
@@ -0,0 +1,32 @@
+// for을 두 번 쓴다고 해서 O(N2) 가 아니다.
+
+function solution(A) {
+  const circleArr = [];
+  let returnVal = 0;
+
+  for (let i = 0; i < A.length; i++) {
+    circleArr.push({
+      lower: i - A[i],
+      upper: i + A[i],
+    });
+  }
+
+  circleArr.sort((a, b) => a.lower - b.lower);
+
+  for (let i = 0; i < A.length; i++) {
+    for (let j = i + 1; j < A.length; j++) {
+      if (returnVal > 10000000) return -1;
+      if (circleArr[j].lower > circleArr[i].upper) break;
+      else if (
+        circleArr[j].lower >= circleArr[i].lower &&
+        circleArr[j].lower <= circleArr[i].upper
+      ) {
+        returnVal++;
+      }
+    }
+  }
+
+  return returnVal;
+}
+
+console.log(solution([1, 5, 2, 1, 4, 0]));
