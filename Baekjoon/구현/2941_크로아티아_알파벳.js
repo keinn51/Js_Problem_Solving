1b6487e07dd42d2d8ab075f2ccce608b71518ae1
keinn51 <soogood97@gmail.com>
2022-12-30 18:10:37 +0900

    2941_크로아티아_알파벳

--- Baekjoon/구현/2941_크로아티아_알파벳.js
+++ Baekjoon/구현/2941_크로아티아_알파벳.js
@@ -1,5 +1,38 @@
-const input = require('fs').readFileSync('text.txt').toString().trim().split("\n");
+const input = require('fs').readFileSync('text.txt').toString().trim();
 
-// input.shift();
-// input.sort((a,b) => a-b)
-// console.log(input.join('\n'))
+const checkWord = {
+    'c':['=', '-'],
+    'd':['z=', '-'],
+    'l': ['j'],
+    'n': ['j'],
+    's': ['='],
+    'z': ['=']
+}
+
+let result = 0;
+
+for (let i =0;i<input.length;i++){
+    // 객체에 없는 경우 보냄
+    if (checkWord[input[i]] === undefined) {
+        result += 1;
+        continue;
+    }
+    // 'dz='만 세 글자이므로 예외처리
+    if (input[i] === 'd') {
+        if (input[i+1] === 'z' && input[i+2] === '=') {
+            i += 2;
+            result += 1;
+            continue;
+        }
+    }
+    const checkNum = checkWord[input[i]].indexOf(input[i+1]);
+    // 우연히 문자가 같은 경우는 보냄
+    if (checkNum === -1) {
+        result += 1;
+        continue;
+    }
+    i += 1;
+    result += 1;
+}
+
+console.log(result)
