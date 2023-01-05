82914f5fa5b1d6f063812b9a1ef8f46d0f0c5b4d
Kyungsle <soogood97@gmail.com>
2023-01-05 10:25:50 +0900

    2504 괄호의 값 getResult 함수 제작

--- Baekjoon/Stack/2504_괄호의값.js
+++ Baekjoon/Stack/2504_괄호의값.js
@@ -40,5 +40,51 @@ function getMultiplied(str) {
 
 console.log(getMultiplied("[[]]"));
 console.log(getMultiplied("()"));
-// console.log(getMultiplied("[]"));
-// console.log(getMultiplied(""));
+
+let result = 0;
+
+function getResult(string) {
+  let nowStr = string;
+  let stackStr = "";
+  const stackArr = [];
+  let idx = 0;
+  const braceCount = {
+    small: 0,
+    middle: 0,
+  };
+  while (nowStr !== "") {
+    switch (nowStr[idx]) {
+      case "(":
+        braceCount["small"] += 1;
+        break;
+      case ")":
+        braceCount["small"] -= 1;
+        break;
+      case "[":
+        braceCount["middle"] += 1;
+        break;
+      case "]":
+        braceCount["middle"] -= 1;
+        break;
+    }
+    stackStr = stackStr.concat(nowStr[idx]);
+    if (braceCount["small"] === 0 && braceCount["middle"] === 0) {
+      nowStr = nowStr.slice(idx + 1, nowStr.length);
+      idx = 0;
+      stackArr.push(stackStr);
+      console.log("stackArr", stackArr);
+      stackStr = "";
+      console.log("nowStr", nowStr);
+      continue;
+    }
+    idx++;
+  }
+  if (stackArr.length > 1) {
+    stackArr.forEach((str) => getResult(str));
+  } else {
+    result += getMultiplied(stackArr[0]);
+  }
+}
+
+getResult(input);
+console.log("result", result);
