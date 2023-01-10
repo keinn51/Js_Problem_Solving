56964fb211c78174ac9b87771684c4040281ba52
Kyungsle <soogood97@gmail.com>
2023-01-10 13:12:08 +0900

    택배 배달과 수거하기 완료

--- programmers/LV2/택배_배달과_수거하기.js
+++ programmers/LV2/택배_배달과_수거하기.js
@@ -5,13 +5,21 @@ function findLastIndexForArray(arr) {
     return -1;
 }
 
+function checkManyParcel(cap, deliveries) {
+    let result = 0;
+    for (let i = 0; i < deliveries.length; i++) {
+        if (deliveries[i] !== 0) result += deliveries[i];
+        if (result > cap) return cap;
+    }
+    return result;
+}
+
 function solution(cap, n, deliveries, pickups) {
     let answer = 0;
     let lastDeliIdx = findLastIndexForArray(deliveries);
     let lastPickIdx = findLastIndexForArray(pickups);
-    // console.log("#1", lastDeliIdx, lastPickIdx);
     while (lastDeliIdx >= 0 && lastPickIdx >= 0) {
-        let iterNum = cap;
+        let iterNum = checkManyParcel(cap, deliveries);
         answer += 2 * (Math.max(lastDeliIdx, lastPickIdx) + 1);
 
         while (iterNum > 0 && lastDeliIdx >= 0) {
@@ -22,7 +30,6 @@ function solution(cap, n, deliveries, pickups) {
                 lastDeliIdx -= 1;
             }
         }
-        // console.log("#3", lastDeliIdx, iterNum);
         while (iterNum < cap && lastPickIdx >= 0) {
             if (pickups[lastPickIdx] > 0) {
                 pickups[lastPickIdx] -= 1;
@@ -33,13 +40,10 @@ function solution(cap, n, deliveries, pickups) {
         }
         if (deliveries[lastDeliIdx] === 0) lastDeliIdx = findLastIndexForArray(deliveries);
         if (pickups[lastPickIdx] === 0) lastPickIdx = findLastIndexForArray(pickups);
-        // console.log("#4", lastPickIdx, iterNum);
-        // console.log("del last", deliveries[lastDeliIdx], "pick last", pickups[lastPickIdx]);
-        // console.log("answer", answer);
     }
     if (lastDeliIdx !== -1) {
         while (lastDeliIdx >= 0) {
-            let iterNum = cap;
+            let iterNum = checkManyParcel(cap, deliveries);
             answer += 2 * (lastDeliIdx + 1);
             while (iterNum > 0 && lastDeliIdx >= 0) {
                 if (deliveries[lastDeliIdx] > 0) {
@@ -78,3 +82,6 @@ console.log("test5", solution(2, 2, [1, 0], [1, 0]), "answer", 2);
 console.log("test6", solution(2, 2, [2, 2], [2, 2]), "answer", 6);
 console.log("test7", solution(2, 2, [3, 3], [3, 3]), "answer", 10);
 console.log("test8", solution(5, 5, [15, 20, 25, 0, 35], [10, 0, 5, 35, 20]), "answer", 130);
+console.log("test9", solution(2, 2, [0, 0], [0, 4]), "answer", 8);
+console.log("test10", solution(1, 4, [0, 2, 3, 1], [0, 0, 0, 0]), "answer", 34);
+console.log("test11", solution(3, 2, [2, 0], [0, 3]), "answer", 4); //찾았다..
