cf60dca21ea55e887e0686a0827fcaaab1b70d72
keinn51 <soogood97@gmail.com>
2023-01-15 02:12:26 +0900

    양궁 대회 첫 트라이 도전

--- programmers/LV2/양궁대회.js
+++ programmers/LV2/양궁대회.js
@@ -1,32 +1,6 @@
 const tempN = 9;
 const tempInfo = [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1];
 
-const getVisitedArr = (idx, visitArr, profitArr, sumScore) => {
-    /**
-     * arrow가 줄어드는 조건 추가해주고 도전해봄
-     */
-    let sum = 0;
-    while (profitArr[idx] >= profitArr[idx + 1]) {
-        visitArr.push(idx);
-        sum += sumScore[idx];
-        idx++;
-    }
-    const firstTryArr = [];
-    const secondTryArr = [];
-    let firstSum = 0,
-        secondSum = 0;
-    firstSum = getVisitedArr(idx, firstTryArr, profitArr, sumScore);
-    secondSum = getVisitedArr(idx + 1, secondTryArr, profitArr, sumScore);
-    if (firstSum >= secondSum) {
-        visitArr.push([...firstTryArr]);
-        sum += firstSum;
-    } else {
-        visitArr.push([...secondTryArr]);
-        sum += secondSum;
-    }
-    return sum;
-};
-
 function solution(n, info) {
     const originScore = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
     const newScore = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0];
@@ -41,9 +15,32 @@ function solution(n, info) {
     console.log("profitArr", profitArr);
     const visitArr = [];
 
-    profitArr.forEach((profit, idx) => {
-        getVisitedArr(visitArr, idx);
-    });
+    const getVisitedArr = (idx, currArrow) => {
+        /**
+         * arrow가 줄어드는 조건 추가해주고 도전해봄
+         */
+        let sum = 0;
+        while (profitArr[idx] >= profitArr[idx + 1] && currArrow >= neededArrowArr[idx]) {
+            visitArr.push(idx);
+            sum += sumScore[idx];
+            currArrow -= neededArrowArr[idx];
+            idx++;
+        }
+        const firstTryArr = [];
+        const secondTryArr = [];
+        let firstSum = 0,
+            secondSum = 0;
+        firstSum = getVisitedArr(idx, currArrow);
+        secondSum = getVisitedArr(idx + 1, currArrow);
+        if (firstSum >= secondSum) {
+            visitArr.push([...firstTryArr]);
+            sum += firstSum;
+        } else {
+            visitArr.push([...secondTryArr]);
+            sum += secondSum;
+        }
+        return sum;
+    };
 }
 
 solution(tempN, tempInfo);
