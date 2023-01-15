6b3cae0c467bba0825de6884864f10108857f6e1
keinn51 <soogood97@gmail.com>
2023-01-15 13:48:12 +0900

    양궁대회 실패 시간이 너무 오래 걸려서 더 하는게 의미 없을듯

--- programmers/LV2/양궁대회.js
+++ programmers/LV2/양궁대회.js
@@ -8,18 +8,21 @@ function solution(n, info) {
         if (info[idx] !== 0) return newScore[idx];
         else return originScore[idx];
     });
-    console.log("sumScore", sumScore);
+    // console.log("sumScore", sumScore);
     const neededArrowArr = info.map((arrow) => arrow + 1);
-    console.log("neededArrowArr", neededArrowArr);
+    // console.log("neededArrowArr", neededArrowArr);
     const profitArr = sumScore.map((score, idx) => score / neededArrowArr[idx]);
-    console.log("profitArr", profitArr);
+    // console.log("profitArr", profitArr);
     const visitArr = [];
 
     const getVisitedArr = (idx, currArrow, visitArr) => {
         let sum = 0;
         // console.log("currArrow", currArrow, neededArrowArr[idx]);
-        while (profitArr[idx] >= profitArr[idx + 1] && currArrow >= neededArrowArr[idx]) {
-            console.log("idx", idx);
+        while (currArrow >= neededArrowArr[idx]) {
+            // console.log("idx", idx);
+            if (profitArr[idx] < profitArr[idx + 1]) {
+                break;
+            }
             visitArr.push(idx);
             sum += sumScore[idx];
             currArrow -= neededArrowArr[idx];
@@ -28,7 +31,15 @@ function solution(n, info) {
         if (currArrow < neededArrowArr[idx]) {
             while (currArrow < neededArrowArr[idx]) {
                 idx++;
-                if (idx >= neededArrowArr.length) break;
+                if (idx >= neededArrowArr.length && currArrow !== 0) {
+                    sum += sumScore[sumScore.length - 1];
+                    visitArr.push(neededArrowArr.length - 1);
+                    return sum;
+                }
+            }
+            if (idx >= neededArrowArr.length) return sum;
+            if (currArrow >= neededArrowArr[idx]) {
+                sum += getVisitedArr(idx, currArrow, visitArr);
             }
         }
         if (profitArr[idx] < profitArr[idx + 1]) {
@@ -39,7 +50,7 @@ function solution(n, info) {
             // console.log("here");
             firstSum += getVisitedArr(idx + 1, currArrow - neededArrowArr[idx], firstTryArr);
             secondSum += getVisitedArr(idx + 1, currArrow, secondTryArr);
-            console.log("firstSum", firstSum, "secondSum", secondSum);
+            // console.log("firstSum", firstSum, "secondSum", secondSum);
             if (firstSum >= secondSum) {
                 visitArr.push(...firstTryArr);
                 sum += firstSum;
@@ -52,15 +63,41 @@ function solution(n, info) {
     };
 
     getVisitedArr(0, n, visitArr);
-    console.log("visitArr", visitArr);
+    // console.log("newSum", newSum);
+    // console.log("visitArr", visitArr);
     const resultArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let curr = n;
     visitArr.forEach((idx) => {
-        resultArr[idx] = neededArrowArr[idx];
+        if (curr >= neededArrowArr[idx]) {
+            curr -= neededArrowArr[idx];
+            resultArr[idx] = neededArrowArr[idx];
+        } else {
+            resultArr[idx] = curr;
+            curr = 0;
+        }
     });
-    console.log("result", resultArr);
+    // console.log("😃result", resultArr);
+
+    const originSum = originScore.reduce((prev, curr, idx) => {
+        // console.log(prev, curr);
+        if (info[idx] >= resultArr[idx] && info[idx] !== 0) {
+            return prev + curr;
+        }
+        return prev;
+    }, 0);
+    const newSum = originScore.reduce((prev, curr, idx) => {
+        // console.log(prev, curr);
+        if (resultArr[idx] > info[idx] && resultArr[idx] !== 0) {
+            return prev + curr;
+        }
+        return prev;
+    }, 0);
+    // console.log("originSum", originSum);
+    if (newSum > originSum) return resultArr;
+    return [-1];
 }
 
-solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]);
-// solution(tempN, tempInfo);
-// solution(tempN, tempInfo);
-// solution(tempN, tempInfo);
+console.log(solution(5, [2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]));
+console.log(solution(1, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+console.log(solution(tempN, tempInfo));
+console.log(solution(10, [0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3]));
