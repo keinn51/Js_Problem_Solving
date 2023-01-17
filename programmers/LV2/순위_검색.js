8e5381bfa3a90d8cbdfa1ac96de4f478af4deee8
keinn51 <soogood97@gmail.com>
2023-01-17 21:55:28 +0900

    순위 검색 효율성 빼고 구현

---
+++ programmers/LV2/순위_검색.js
@@ -0,0 +1,41 @@
+function solution(info, query) {
+    const newInfo = [];
+    info.forEach((str) => {
+        const [language, group, career, food, grade] = str.split(" ");
+        newInfo.push({ language, group, career, food, grade });
+    });
+    console.log("newInfo", newInfo);
+    return query.map((str) => {
+        const [language, group, career, foodAndgrade] = str.split(" and ");
+        const [food, grade] = foodAndgrade.split(" ");
+        return newInfo.filter(
+            (obj) =>
+                (language === "-" || language === obj.language) &&
+                (group === "-" || group === obj.group) &&
+                (career === "-" || career === obj.career) &&
+                (food === "-" || food === obj.food) &&
+                (grade === "-" || Number(obj.grade) >= Number(grade))
+        ).length;
+    });
+}
+
+console.log(
+    solution(
+        [
+            "java backend junior pizza 150",
+            "python frontend senior chicken 210",
+            "python frontend senior chicken 150",
+            "cpp backend senior pizza 260",
+            "java backend junior chicken 80",
+            "python backend senior chicken 50",
+        ],
+        [
+            "java and backend and junior and pizza 100",
+            "python and frontend and senior and chicken 200",
+            "cpp and - and senior and pizza 250",
+            "- and backend and senior and - 150",
+            "- and - and - and chicken 100",
+            "- and - and - and - 150",
+        ]
+    )
+);
