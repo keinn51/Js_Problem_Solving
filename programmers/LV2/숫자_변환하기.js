4cfaebf67ddf40bff4aa6905ccdaa19ac42d97fe
keinn51 <soogood97@gmail.com>
2023-02-09 00:24:08 +0900

    숫자 변환하기 BFS 런타임 에러

--- programmers/LV2/숫자_변환하기.js
+++ programmers/LV2/숫자_변환하기.js
@@ -1,20 +1,24 @@
 function solution(x, y, n) {
-    let res = -1;
-    function iter(dx, y, n, depth) {
-        if (res !== -1 && depth >= res) return;
-        if (dx < y) {
-            iter(dx + n, y, n, depth + 1);
-            iter(dx * 2, y, n, depth + 1);
-            iter(dx * 3, y, n, depth + 1);
+    let needVisitArr = [x];
+    let depth = 0;
+    while (needVisitArr.length > 0) {
+        const newArr = [];
+        // console.log(needVisitArr);
+        for (let i = 0; i < needVisitArr.length; i++) {
+            const nowElem = needVisitArr[i];
+            if (nowElem === y) return depth;
+            if (nowElem > y) continue;
+            if (nowElem < y - n) newArr.push(nowElem + n);
+            if (nowElem === y - n) return depth + 1;
+            if (nowElem * 2 < y) newArr.push(nowElem * 2);
+            if (nowElem * 2 === y) return depth + 1;
+            if (nowElem * 3 < y) newArr.push(nowElem * 3);
+            if (nowElem * 3 === y) return depth + 1;
         }
-        if (dx === y) {
-            if (res === -1) res = depth;
-            else res = Math.min(res, depth);
-        }
-        return;
+        needVisitArr = [...newArr];
+        depth += 1;
     }
-    iter(x, y, n, 0);
-    return res;
+    return -1;
 }
 
 console.log(solution(10, 40, 5));
