8837c8a6af68895a1b743a36d7d06d885a0f4c9a
keinn51 <soogood97@gmail.com>
2023-02-11 15:16:56 +0900

    마법의 엘리베이터 오류 해결

--- programmers/LV2/마법의_엘리베이터.js
+++ programmers/LV2/마법의_엘리베이터.js
@@ -1,20 +1,23 @@
 function solution(storey) {
     const storeyArr = ("0" + new String(storey)).split("").map((e) => +e);
     for (let i = storeyArr.length - 1; i >= 1; i--) {
-        if (storeyArr[i] > 5) {
+        if (storeyArr[i] > 5 || (storeyArr[i] === 5 && storeyArr[i - 1] >= 5)) {
             storeyArr[i - 1] += 1;
             storeyArr[i] = 10 - storeyArr[i];
         }
     }
-    console.log(storey, storeyArr);
     return storeyArr.reduce((prev, curr) => prev + curr, 0);
 }
 
-console.log(solution(16));
-console.log(solution(2556));
-console.log(solution(6556));
-console.log(solution(5555));
-console.log(solution(1111));
-console.log(solution(1));
-console.log(solution(7777));
-console.log(solution(123456789));
+console.log("res", solution(16));
+console.log("res", solution(2554));
+// console.log("res", solution(1));
+// console.log("res", solution(10));
+// console.log("res", solution(100));
+// console.log("res", solution(1000));
+// console.log("res", solution(100000000));
+// console.log("res", solution(9));
+// console.log("res", solution(999));
+// console.log("res", solution(999999999));
+// console.log("res", solution(666));
+console.log("res", solution(755));
