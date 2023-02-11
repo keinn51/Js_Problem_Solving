d5284e0f8bf321fe9cd1d8f769b571f354c990cc
keinn51 <soogood97@gmail.com>
2023-02-11 22:30:38 +0900

    이분 탐색으로 해봄

--- programmers/LV2/디펜스_게임.js
+++ programmers/LV2/디펜스_게임.js
@@ -1,37 +1,21 @@
-function binaryInsert(arr, num) {
-    let startIdx = 0;
-    let endIdx = arr.length - 1;
-    let midIdx = Math.floor((startIdx + endIdx) / 2);
-    while (startIdx <= endIdx) {
-        if (num === arr[midIdx]) {
-            arr.splice(midIdx, 0, num);
-            return;
-        }
-        if (num < arr[midIdx]) endIdx = midIdx - 1;
-        else startIdx = midIdx + 1;
-        midIdx = Math.floor((startIdx + endIdx) / 2);
-    }
-    arr.splice(midIdx + 1, 0, num);
-}
-
 function solution(n, k, enemy) {
-    // 2) Heap 객체를 생성한다.
-    let heap = [];
-    let answer = k;
-
-    heap = enemy.slice(0, k).sort((a, b) => a - b);
-    for (let i = k; i < enemy.length; i++) {
-        let min = heap[0];
-        if (enemy[i] > min) {
-            n -= min;
-            heap.shift();
-            binaryInsert(heap, enemy[i]);
-        } else {
-            n -= enemy[i];
-        }
-        if (n >= 0) answer++;
+    let [left, right] = [0, enemy.length];
+    let mid = Math.floor((left + right) / 2);
+    while (left <= right) {
+        const curSlice = enemy.slice(0, mid).sort((a, b) => b - a);
+        let noDie = k;
+        const curEnemy = curSlice.reduce((acc, cur) => {
+            if (noDie > 0) {
+                noDie--;
+                return acc;
+            }
+            return acc + cur;
+        }, 0);
+        if (n >= curEnemy) left = mid + 1;
+        else right = mid - 1;
+        mid = Math.floor((left + right) / 2);
     }
-    return answer;
+    return left - 1;
 }
 
 // let temp = [2, 4, 25, 33, 100];
