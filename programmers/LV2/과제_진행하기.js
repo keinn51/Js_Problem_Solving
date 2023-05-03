0a04558aed8e3b4f80a7d0e4105393721ffb7d5b
keinn51 <soogood97@gmail.com>
2023-05-03 20:20:28 +0900

    과제 진행하기 두 번째 시도

--- programmers/LV2/과제_진행하기.js
+++ programmers/LV2/과제_진행하기.js
@@ -1,3 +1,46 @@
+// function solution(plans) {
+//     plans.sort((a, b) => -compareTime(a[1], b[1]));
+//     const todoStack = [],
+//         result = [];
+//     let nowTime = plans[0][1],
+//         doing = plans[0];
+//     // console.log(plans);
+//     plans.forEach((newSubject, idx) => {
+//         if (idx === 0) return;
+//         // 하던 것을 마무리 할 수 없을 때
+//         if (compareTime(addTime(doing[1], doing[2]), newSubject[1]) < 0) {
+//             todoStack.push([doing[0], doing[2] - minusTime(newSubject[1], doing[1])]);
+//             nowTime = newSubject[1];
+//         }
+//         // 하던 것 마무리~
+//         else {
+//             result.push(doing[0]);
+//             nowTime = addTime(nowTime, doing[2]);
+//             // 미뤄둔 것 할 짬이 되나 확인하기
+//             while (todoStack.length) {
+//                 const recentlyStack = todoStack[todoStack.length - 1];
+//                 if (compareTime(addTime(nowTime, recentlyStack[1]), newSubject[1]) < 0) {
+//                     recentlyStack[1] -= minusTime(newSubject[1], nowTime);
+//                     nowTime = newSubject[1];
+//                     // console.log(todoStack);
+//                     break;
+//                 }
+//                 result.push(recentlyStack[0]);
+//                 nowTime = addTime(nowTime, recentlyStack[1]);
+//                 todoStack.pop();
+//             }
+//         }
+//         doing = newSubject;
+//     });
+//     result.push(doing[0]);
+//     while (todoStack.length) {
+//         const todo = todoStack[todoStack.length - 1];
+//         result.push(todo[0]);
+//         todoStack.pop();
+//     }
+//     return result;
+// }
+
 function addTime(time, min) {
     const timeDate = new Date();
     const [timeH, timeM] = time.split(":");
@@ -29,44 +72,34 @@ function compareTime(time1, time2) {
 }
 
 function solution(plans) {
-    plans.sort((a, b) => -compareTime(a[1], b[1]));
-    const todoStack = [],
-        result = [];
-    let nowTime = plans[0][1],
-        doing = plans[0];
-    // console.log(plans);
-    plans.forEach((newSubject, idx) => {
-        if (idx === 0) return;
-        // 하던 것을 마무리 할 수 없을 때
-        if (compareTime(addTime(doing[1], doing[2]), newSubject[1]) < 0) {
-            todoStack.push([doing[0], doing[2] - minusTime(newSubject[1], doing[1])]);
-            nowTime = newSubject[1];
+    plans.sort((a, b) => compareTime(a[1], b[1]));
+    let nowTime = plans[plans.length - 1][1];
+    const subStack = [plans[plans.length - 1][0]],
+        minStack = [plans[plans.length - 1][2]],
+        result = [],
+        resLen = plans.length;
+    plans.pop();
+    while (result.length < resLen) {
+        const newDo = plans[plans.length - 1];
+        const [newSub, newTime, newMin] = newDo || [null, null, null];
+        if (
+            newDo &&
+            (minStack.length === 0 ||
+                compareTime(addTime(nowTime, minStack[minStack.length - 1]), newTime) < 0)
+        ) {
+            if (minStack.length !== 0) minStack[minStack.length - 1] -= minusTime(newTime, nowTime);
+            minStack.push(newMin);
+            subStack.push(newSub);
+            nowTime = newTime;
+            plans.pop();
+        } else {
+            const addedTime = minStack[minStack.length - 1];
+            const endSub = subStack[subStack.length - 1];
+            result.push(endSub);
+            nowTime = addTime(nowTime, addedTime);
+            minStack.pop();
+            subStack.pop();
         }
-        // 하던 것 마무리~
-        else {
-            result.push(doing[0]);
-            nowTime = addTime(nowTime, doing[2]);
-            // 미뤄둔 것 할 짬이 되나 확인하기
-            while (todoStack.length) {
-                const recentlyStack = todoStack[todoStack.length - 1];
-                if (compareTime(addTime(nowTime, recentlyStack[1]), newSubject[1]) < 0) {
-                    recentlyStack[1] -= minusTime(newSubject[1], nowTime);
-                    nowTime = newSubject[1];
-                    // console.log(todoStack);
-                    break;
-                }
-                result.push(recentlyStack[0]);
-                nowTime = addTime(nowTime, recentlyStack[1]);
-                todoStack.pop();
-            }
-        }
-        doing = newSubject;
-    });
-    result.push(doing[0]);
-    while (todoStack.length) {
-        const todo = todoStack[todoStack.length - 1];
-        result.push(todo[0]);
-        todoStack.pop();
     }
     return result;
 }
