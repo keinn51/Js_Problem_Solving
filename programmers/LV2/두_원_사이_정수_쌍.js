f08a1279e80ebd2f53abc9709245d5e903a9d060
keinn51 <soogood97@gmail.com>
2023-05-06 21:35:21 +0900

    두 원 사이 정수 쌍

--- programmers/LV2/두_원_사이_정수_쌍.js
+++ programmers/LV2/두_원_사이_정수_쌍.js
@@ -1,17 +1,23 @@
 function solution(r1, r2) {
     let result = 0;
     for (let x = -r2; x <= r2; x++) {
-        const start = Math.ceil(-1 * Math.sqrt(9 - Math.pow(x, 2)));
-        const end = Math.floor(Math.sqrt(9 - Math.pow(x, 2)));
-        for (let y = start; y <= end; y++) {
-            result += 1;
+        const oY = Math.sqrt(Math.pow(r2, 2) - Math.pow(x, 2));
+        const oEnd = Math.floor(oY);
+        const oStart = Math.ceil(-oY);
+        let count = oEnd - oStart + 1;
+
+        if (x > -r1 && x < r1) {
+            const iY = Math.sqrt(Math.pow(r1, 2) - Math.pow(x, 2));
+            const iEnd = Math.floor(iY);
+            const iStart = Math.ceil(-iY);
+            if (iEnd === iY) count += 2;
+            count -= iEnd - iStart + 1;
         }
+        result += count;
     }
 
-    console.log("res / minuse", result, Math.pow(2 * r1 - 1, 2));
-    result -= Math.pow(2 * r1 - 1, 2);
     return result;
 }
 
 console.log(solution(2, 3));
-console.log(solution(3, 4));
+// console.log(solution(3, 4));
