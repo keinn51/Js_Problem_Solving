34d065ad0f4127522d428a0fcdb6258092856e72
keinn51 <soogood97@gmail.com>
2023-06-06 14:14:10 +0900

    1991 트리 순회 완료

--- Baekjoon/Tree/1991_트리순회.js
+++ Baekjoon/Tree/1991_트리순회.js
@@ -1,10 +1,7 @@
-const filename = process.platform === "linux" ? "/dev/stdin" : "./text.txt";
+const filename = process.platform === "linux" ? "/dev/stdin" : "text.txt";
 const input = require("fs").readFileSync(filename).toString().split("\n");
 
-/**
- * 트리를 만들고 각 순회 메서드를 구현해본다.
- */
-
+// 트리를 만들고 각 순회 메서드를 구현해본다.
 class BinarySearchNode {
     constructor(value, left, right) {
         this.value = value;
@@ -19,35 +16,35 @@ class BinarySearchTree {
         this.tree = tree;
         this.head = "A";
     }
-    preOrder() {
+    checker(mode) {
+        // mode : pre / in / post
         // DFS를 구현하면 된다
-        const needVisit = [];
         const result = [];
-        needVisit.push(this.head);
-        while (needVisit.length > 0) {
-            const nodeInstance = this.tree.get(needVisit.shift());
-            if (nodeInstance.left) needVisit.push(nodeInstance.left);
-            if (nodeInstance.right) needVisit.push(nodeInstance.right);
-            result.push(nodeInstance.value);
+        function iter(node, tree) {
+            const nodeInstance = tree.get(node);
+            if (mode === "pre") result.push(nodeInstance.value);
+            if (nodeInstance.left) iter(nodeInstance.left, tree);
+            if (mode === "in") result.push(nodeInstance.value);
+            if (nodeInstance.right) iter(nodeInstance.right, tree);
+            if (mode === "post") result.push(nodeInstance.value);
         }
-        return result;
+        iter(this.head, this.tree);
+        return result.join("");
     }
-    inOrder() {}
-    postOrder() {}
 }
 
 const solution = (input) => {
-    const allNodeCnt = Number(input[0]);
-    const tree = new Map();
+    const treeMap = new Map();
     for (let i = 1; i < input.length; i++) {
         const [now, left, right] = input[i].split(" ");
         const _left = left !== "." ? left : null;
         const _right = right !== "." ? right : null;
-        tree.set(now, new BinarySearchNode(now, _left, _right));
+        treeMap.set(now, new BinarySearchNode(now, _left, _right));
     }
-    const binaryTree = new BinarySearchTree(tree);
-    console.log(binaryTree);
-    console.log(binaryTree.preOrder());
+    const binaryTree = new BinarySearchTree(treeMap);
+    console.log(binaryTree.checker("pre"));
+    console.log(binaryTree.checker("in"));
+    console.log(binaryTree.checker("post"));
 };
 
-console.log(solution(input));
+solution(input);
