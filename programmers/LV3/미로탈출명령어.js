1a18d4b9a324e0969d4447174206b0d0535e37be
keinn51 <soogood97@gmail.com>
2023-06-18 11:12:13 +0900

    미로탈출 명령어 정답 코드 확인

--- programmers/LV3/미로탈출명령어.js
+++ programmers/LV3/미로탈출명령어.js
@@ -1,57 +1,48 @@
-/**
- * d l r u
- * 1. 위 네개 문자를 조합해서 나올 수 있는 크기가 k인 문자열의 배열을 구한다.
- * 2. 수식에서 나온 것만 추가한다
- * 3. 문자열이 완성되었을 때 맵을 넘는지 아닌지 구하기
- * 4. 가능성 있는 것들 중에서 sort해서 맨 앞거를 보내기
- */
-function solution(n, m, x, y, r, c, k) {
-    const ulc = (k + (x + y) - (r + c)) / 2;
+const getMinDist = (x, y, r, c) => {
+    return Math.abs(x - r) + Math.abs(y - c);
+};
 
-    if (Number.isInteger(ulc) === false) return "impossible";
-    if (Math.abs(r - x) + Math.abs(c - y) > k) return "impossible";
+const move = (n, m, x, y) => {
+    let dx = [1, 0, 0, -1];
+    let dy = [0, -1, 1, 0];
 
-    const passArr = [];
+    for (let i = 0; i < 4; i++) {
+        let nx = x + dx[i];
+        let ny = y + dy[i];
 
-    function iter(str, px, py) {
-        if (str.length === k) {
-            let dc = 0,
-                lc = 0,
-                rc = 0,
-                uc = 0;
-
-            for (let idx in str) {
-                const word = str[idx];
-                if (word === "d") dc += 1;
-                if (word === "l") lc += 1;
-                if (word === "r") rc += 1;
-                if (word === "u") uc += 1;
-            }
-
-            if (uc + lc === ulc && dc + rc + ulc === k && dc - uc === r - x && rc - lc === c - y)
-                passArr.push(str);
-
-            return;
+        if (nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
+            return i;
         }
-        if (px < n) iter(str + "d", px + 1, py);
-        if (py > 1) iter(str + "l", px, py - 1);
-        if (py < m) iter(str + "r", px, py + 1);
-        if (px > 1) iter(str + "u", px - 1, py);
+    }
+};
+
+function solution(n, m, x, y, r, c, k) {
+    let minDist = getMinDist(x, y, r, c);
+    let directions = ["d", "l", "r", "u"];
+
+    if (minDist % 2 !== k % 2 || k < minDist) return "impossible";
+
+    let answer = "";
+
+    while (minDist < k) {
+        let mIdx = move(n, m, x, y);
+        answer += directions[mIdx];
+
+        if (mIdx === 0) x++;
+        else if (mIdx === 1) y--;
+        else if (mIdx === 2) y++;
+        else x--;
+
+        minDist = getMinDist(x, y, r, c);
+        k--;
     }
 
-    iter("", x, y);
+    if (x < r) answer += "d".repeat(r - x);
+    if (y > c) answer += "l".repeat(y - c);
+    if (y < c) answer += "r".repeat(c - y);
+    if (x > r) answer += "u".repeat(x - r);
 
-    passArr.sort((a, b) => {
-        for (let i = 0; i < k; i++) {
-            if (a[i] < b[i]) return -1;
-            else if (a[i] > b[i]) return 1;
-        }
-        return 0;
-    });
-
-    console.log(passArr);
-
-    return passArr[0];
+    return answer;
 }
 
 console.log(solution(3, 4, 2, 3, 3, 1, 5));
