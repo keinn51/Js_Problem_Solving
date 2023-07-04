d94fec5cda39ea63175ffce21bbbf5ead6bd89bb
keinn51 <soogood97@gmail.com>
2023-07-04 23:50:20 +0900

    1244 2차... 출력 형식 왜 그렇게 하는거니??

--- Baekjoon/Implementation/1244_스위치켜고끄기.js
+++ Baekjoon/Implementation/1244_스위치켜고끄기.js
@@ -9,42 +9,30 @@ for (let i = 3; i < input.length; i++) {
 }
 
 function solution(lights, students) {
-    /**
-     * 남학생은... 그냥 배수를 돌면 되기는 함
-     * -> 1이 나온다면 영락없이 On2이 되고 말음
-     * 100 100 이면 On2도 될듯...?
-     *
-     * 남학생은 어차피 무조건 바뀌는 거니까 한 번에 모아서 는 안 됨 여자는 아니어서
-     * 그냥 순서대로 해야 하는데...?
-     */
     for (let i = 0; i < students.length; i++) {
-        // i 01234567
         const [_sex, _lnum] = students[i];
         if (_sex === 1) {
-            // j 12345678
-            // lnum 3
             for (let j = 1; _lnum * j <= lights.length - 1; j++) {
                 lights[_lnum * j] = lights[_lnum * j] === 1 ? 0 : 1;
             }
-            console.log(`man`, lights);
         } else if (_sex === 2) {
-            // 4 -> 23 -> 15 -> end
-            // left <= 0 || right >=students.length -> exit
             for (let j = 0; _lnum - j >= 1 && _lnum + j <= lights.length - 1; j++) {
-                console.log(`kyungsle`, lights, _lnum - j, j);
                 if (lights[_lnum - j] === lights[_lnum + j]) {
                     lights[_lnum - j] = lights[_lnum - j] === 1 ? 0 : 1;
                     if (j === 0) continue;
                     lights[_lnum + j] = lights[_lnum + j] === 1 ? 0 : 1;
                 } else break;
             }
-            console.log(`woman`, lights);
         }
     }
-    lights.shift();
-    console.log(lights);
+
+    let res = "";
+    for (let i = 1; i < lights.length; i++) {
+        res += String(lights[i]);
+        if (i % 20 === 0) res += "\n";
+        else res += " ";
+    }
+    console.log(res);
 }
 
-console.log([0, ...lights]);
-console.log(students);
-console.log(solution([0, ...lights], students));
+solution([0, ...lights], students);
