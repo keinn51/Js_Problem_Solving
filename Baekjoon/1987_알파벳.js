d2279ad3c09f958ac5602da7f8c2783b899a90f5
keinn51 <soogood97@gmail.com>
2023-09-30 00:44:08 +0900

    1987 알파벳 > 시간초과..

--- Baekjoon/1987_알파벳.js
+++ Baekjoon/1987_알파벳.js
@@ -16,43 +16,38 @@ function solution(input) {
     const [_, ...arr] = input;
     const map = arr.map((l) => l.split(""));
     const vmap = arr.map((_) => new Array(C).fill(0));
-    let needVisit = [[0, 0]];
-    let count = 0;
+    let count = 1;
     const dx = [-1, 0, 1, 0];
     const dy = [0, 1, 0, -1];
-    const mirror = {};
 
-    console.log(map);
+    vmap[0][0] = 1;
 
-    while (needVisit.length > 0) {
-        let newTarget = [];
-        for (let i = 0; i < needVisit.length; i++) {
-            const [_x, _y] = needVisit[i];
-            for (let j = 0; j < 4; j++) {
-                const nx = _x + dx[j];
-                const ny = _y + dy[j];
+    const iter = (_x, _y, mirror, _count) => {
+        count = Math.max(_count, count);
+        mirror[map[_y][_x]] = true;
 
-                if (
-                    nx >= 0 &&
-                    nx < C &&
-                    ny >= 0 &&
-                    ny < R &&
-                    !mirror[map[ny][nx]] &&
-                    vmap[ny][nx] === 0
-                ) {
-                    mirror[map[ny][nx]] = true;
-                    vmap[ny][nx] = 1;
-                    console.log(nx, ny, vmap.join("\n"));
-                    newTarget.push([nx, ny]);
-                }
+        for (let j = 0; j < 4; j++) {
+            const nx = _x + dx[j];
+            const ny = _y + dy[j];
+
+            if (
+                nx >= 0 &&
+                nx < C &&
+                ny >= 0 &&
+                ny < R &&
+                !mirror[map[ny][nx]] &&
+                vmap[ny][nx] === 0
+            ) {
+                vmap[ny][nx] = 1;
+                iter(nx, ny, { ...mirror }, _count + 1);
+                vmap[ny][nx] = 0;
             }
         }
-        needVisit = [...newTarget];
-        newTarget = null;
-        count += 1;
-    }
+    };
 
-    console.log(vmap, count);
+    iter(0, 0, {}, 1);
+
+    return console.log(count);
 }
 
 solution(
