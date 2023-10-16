384d83107be7e2e9d9717437c17d66b060d458a6
keinn51 <soogood97@gmail.com>
2023-10-16 23:48:06 +0900

    2156 포도주시식 어렵군 완탐은 아닌데 dp인 거슨 확실

---
+++ Baekjoon/2156_포도주시식.js
@@ -0,0 +1,58 @@
+/**
+ * 연속 두 개를 골라서 최대한으로 가져갈 수 있는 것을 체크하기
+ *
+ * 6, 10, 13, 9, 8, 1
+ * 
+ * 300 300 100 1 300 300 1 1 300 300
+ * 
+ * 300 300 200 300 1 1 300 1 300 300
+ *
+ * 6 10 / 9 8  => 33
+ * 10 13 / 8 1  => 31
+ *
+ * 6 16 29 38 46 47
+ * 10과 6을 선택하면 9-8 혹은 8-1을 선택 가능 13 9 는 선택 불가
+ * 근데.. 하나만 선택할 수도 있지
+ *
+ * 완탐인걸까? dp인걸까
+ *
+ * 6 10 13 중에서 보자면
+ * 6 13 : 19
+ * 6 10 : 16
+ * 10 13 : 23
+ *
+ * 9 8 1 중에서 보자면
+ * 9 1 : 10
+ * 9 8 : 17
+ * 8 1 : 9
+ */
+function solution(input) {
+    const [a, ...b] = input;
+    const N = Number(a);
+    const arr = b.map(Number);
+    const stack = [0, 0, 0];
+    console.log(N, arr);
+
+    for (let i = 0; i < N; i += 3) {
+        const [f, s, t] = [arr[i], arr[i + 1], arr[i + 2]];
+        
+        if (f !== undefined) {
+            if (s !== undefined) {
+                stack[0] += (f+)
+                if (t !== undefined) {
+
+                }
+            }
+        }
+    }
+}
+
+solution(
+    require("fs")
+        .readFileSync(process.platform === "linux" ? "/dev/stdin" : "text.txt")
+        .toString()
+        .trim()
+        .split("\n")
+);
+
+module.exports = solution;
