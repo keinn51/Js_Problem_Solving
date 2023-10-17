b4d1c52ffe17ac9241419ddbd1277bdde3d563c7
keinn51 <soogood97@gmail.com>
2023-10-17 20:10:37 +0900

    2156 포도주 시식 거의 다 왔다.. 세개씩 하는게 아니다!

--- Baekjoon/2156_포도주시식.js
+++ Baekjoon/2156_포도주시식.js
@@ -2,9 +2,9 @@
  * 연속 두 개를 골라서 최대한으로 가져갈 수 있는 것을 체크하기
  *
  * 6, 10, 13, 9, 8, 1
- * 
+ *
  * 300 300 100 1 300 300 1 1 300 300
- * 
+ *
  * 300 300 200 300 1 1 300 1 300 300
  *
  * 6 10 / 9 8  => 33
@@ -31,20 +31,20 @@ function solution(input) {
     const N = Number(a);
     const arr = b.map(Number);
     const stack = [0, 0, 0];
-    console.log(N, arr);
 
     for (let i = 0; i < N; i += 3) {
-        const [f, s, t] = [arr[i], arr[i + 1], arr[i + 2]];
-        
-        if (f !== undefined) {
-            if (s !== undefined) {
-                stack[0] += (f+)
-                if (t !== undefined) {
+        const [pf, ps, pt] = stack;
+        console.log("stack", stack);
+        const f = arr[i];
+        const s = i + 1 < N ? arr[i + 1] : 0;
+        const t = i + 2 < N ? arr[i + 2] : 0;
 
-                }
-            }
-        }
+        stack[0] = pf + f + s;
+        stack[1] = Math.max(pf, ps) + f + t;
+        stack[2] = Math.max(pf, ps, pt) + s + t;
     }
+
+    console.log(Math.max(...stack));
 }
 
 solution(
